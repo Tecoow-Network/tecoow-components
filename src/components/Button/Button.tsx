62e07746ff99a3216c8f506ecda21dda171d0e71
TeodorescuCostin <costin.teodorescu5@gmail.com>
2024-07-03 20:43:04 +0300

    Fields and functionallity for button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,10 +2,38 @@ import React from 'react';
 
 interface ButtonProps {
     label: string;
+    onClick?: () => void;
+    style?: React.CSSProperties;
+    className?: string;
+    type?: 'button' | 'submit' | 'reset';
+    disabled?: boolean;
+    ariaLabel?: string;
+    id?: string;
+    title?: string;
+    autoFocus?: boolean;
+    tabIndex?: number;
+    dataTestId?: string;
+
 }
 
 const Button = (props: ButtonProps) => {
-    return <button>{props.label}</button>;
+    return (
+        <button
+            onClick={props.onClick}
+            style={props.style}
+            className={props.className}
+            type={props.type}
+            disabled={props.disabled}
+            aria-label={props.ariaLabel}
+            id={props.id}
+            title={props.title}
+            autoFocus={props.autoFocus}
+            tabIndex={props.tabIndex}
+            data-testid={props.dataTestId}
+        >
+            {props.label}
+        </button>
+    );
 };
 
 export default Button;
