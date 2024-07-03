d4e3533cd0d82efa5d4ba728cd674dfaf974b2ad
TeodorescuCostin <costin.teodorescu5@gmail.com>
2024-07-03 22:57:06 +0300

    Input text field, functionality

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -3,3 +3,4 @@ export { default as InputDate } from './InputDate';
 export { default as InputEmail } from './InputEmail';
 export { default as InputNumber } from './InputNumber';
 export { default as InputPassword } from './InputPassword';
+export { default as InputText } from './InputText';
